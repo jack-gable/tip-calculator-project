418e0fb2705fbcb94a0673c8b97c740db93a2a9b
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2024-01-21 15:05:30 -0800

    initial commit files

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import styles from "./Title.module.css";
+
+function Title() {
+	return (
+		<div style={styles.title}>
+			<span>Spli</span>
+			<span>tter</span>
+		</div>
+	);
+}
+
+export default Title;
